a1dfb1abdbd93683fb29920bcd9bcc15c876fe63
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2014-03-04 18:11:35 -0600

    Create a basic home page featuring doge

--- dogehouse/dogehouse/settings.py
+++ dogehouse/dogehouse/settings.py
@@ -86,3 +86,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+

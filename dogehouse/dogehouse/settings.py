db55e5d2ccea6967b049b8aea7ed067264868557
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2014-03-04 20:39:14 -0600

    Configure the project for Heroku deployment

--- dogehouse/dogehouse/settings.py
+++ dogehouse/dogehouse/settings.py
@@ -89,6 +89,28 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'dogehouse/static'),
 )
 
+
+# HEROKU config
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(default='postgres://arash:@localhost/dogefact')
+
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)

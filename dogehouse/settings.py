61d36da8e80e05b339dadf05decdc1662d99211d
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2014-04-01 02:07:58 -0500

    Only use S3 CDN when debug is inactive

--- dogehouse/settings.py
+++ dogehouse/settings.py
@@ -98,10 +98,10 @@ STATICFILES_DIRS = (
 
 
 # S3 configuration
-#if not DEBUG:
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = S3_URL
+if not DEBUG:
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    STATIC_URL = S3_URL
 
 
 # HEROKU config
